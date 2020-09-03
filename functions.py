90d3020125f79eb5b24c28a1f4ff55c2df15e298
Mike Driver <michael.driver@imaginecommunications.com>
2020-09-03 16:43:49 +0100

    Making readable data and tidying up

--- functions.py
+++ functions.py
@@ -6,15 +6,32 @@
 # investigate:
 # assert isinstance(data, object)
 import json
+from decimal import Decimal
+
 
 def load_json(data):
     pyobject = json.loads(data)
     return pyobject
 
+def print_readable_data(json_dict):
+    kelvin_temp = json_dict['main']['temp']
+    celsius_temp = round(Decimal(kelvin_temp - 273.15), 1)
+
+    feels_like_kelvin_temp = json_dict['main']['feels_like']
+    feels_like_celsius_temp = round(Decimal(feels_like_kelvin_temp - 273.15), 1)
+
+    weather_description = json_dict['weather'][0]['main'] + ", " + json_dict['weather'][0]['description']
+    location = str(json_dict['name']).upper()
+    print(f"weather description in {location}: " + weather_description)
+    print("temp: " + str(celsius_temp))
+    print("feels like temp: " + str(feels_like_celsius_temp))
+    #print("Sunrise: " + str(json_dict['sys']['sunrise']))
+
+
 def get_weather(location, api_key):
     import urllib.request
     weburl = urllib.request.urlopen(f"http://api.openweathermap.org/data/2.5/weather?q={location}&APPID={api_key}")
-    print("result code: " + str(weburl.getcode()))
+    #print("result code: " + str(weburl.getcode()))
     data = weburl.read()
     return data
 
--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 import functions
+from decimal import Decimal
 
 # functions.test_string_printing()
 # functions.some_maths()
@@ -6,30 +7,31 @@ import functions
 
 api_key = functions.file_read_api_key()
 
-data = functions.get_weather('frankfurt',api_key)
-print ('FRANKFURT')
-print(data)
+# data = functions.get_weather('frankfurt',api_key)
+# print ('FRANKFURT')
+# print(data)
+#
+# json_stuff = functions.load_json(data)
+# print('json stuff: ')
+# print (json_stuff)
+# print ("Sunrise: " + str(json_stuff['sys']['sunrise']))
 
-json_stuff = functions.load_json(data)
-print('json stuff: ')
-print (json_stuff)
-print ("Sunrise: " + str(json_stuff['sys']['sunrise']))
-sunrise = json_stuff['sys']['sunrise'] / 86400 + 25569
-print (sunrise)
-
-city_names = ["london","new%20york","cairo","cardiff","paris","madrid", "bangkok","denver","LiverPOol"]
+city_names = ["LONdon","Cardiff","leeds","plymouth","toronto","denver","Longyearbyen","Ushuaia","Kirkwall"]
 for city in city_names:
     data = functions.get_weather(city,api_key)
-    print(city.capitalize())
-    print(data)
+    #print(city.capitalize())
+    #print(data)
+    functions.print_readable_data(functions.load_json(data))
+    print("")
+
 
 city_name = ''
 while city_name != 'quit':
     city_name = input("enter city name:\n")
     if city_name != 'quit':
         data = functions.get_weather(city_name,api_key)
-        print(city_name.upper())
-        print (data)
-
+        # print(city_name.upper())
+        # print (data)
+        functions.print_readable_data(functions.load_json(data))
 
 
