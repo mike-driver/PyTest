23a6392031f644e279c1b72e04cbb2b840afc5b1
Mike Driver <michael.driver@imaginecommunications.com>
2020-09-10 09:29:00 +0100

    Separating functions into another file

--- test1.py
+++ test1.py
@@ -2,17 +2,23 @@ import file_functions
 import weather_functions
 from decimal import Decimal
 
+for x in range(0, 3):
+    print("We're on time %d" % (x))
+    print(f"We're on time {x}")
+
+print("\n")
+
 api_key = file_functions.file_read_api_key()
 sampledata = weather_functions.get_weather("cardiff",api_key)
 print("Sample data:")
 print(str(sampledata))
-print("")
+print("\n")
 
 city_names = ["london","Cardiff","edinburgh","leeds","plymouth","toronto","denver","Longyearbyen","Ushuaia","Kirkwall","Windy%20City"]
 for city in city_names:
     data = weather_functions.get_weather(city,api_key)
     weather_functions.print_readable_data(weather_functions.load_json(data))
-    print("")
+    print("\n")
 
 city_name = ''
 while city_name != 'quit':
--- weather_functions.py
+++ weather_functions.py
@@ -8,9 +8,13 @@
 import json
 from decimal import Decimal
 
+
 def load_json(data):
     pyobject = json.loads(data)
+    #print(f"data::\n {data}")
+    #print(f"pyobject::\n {pyobject}")
     return pyobject
+
 
 def print_readable_data(json_dict):
     kelvin_temp = json_dict['main']['temp']
@@ -20,23 +24,20 @@ def print_readable_data(json_dict):
     wind_strength = json_dict['wind']['speed']
     wind_direction = json_dict['wind']['deg']
     humidity = json_dict['main']['humidity']
-    #print(data)
 
     weather_description = json_dict['weather'][0]['main'] + ", " + json_dict['weather'][0]['description']
     location = str(json_dict['name']).upper()
-    print(f"Weather description in {location}, " + str(json_dict['sys']['country']) +": " + weather_description)
+    print(f"Weather description in {location}, " + str(json_dict['sys']['country']) + ": " + weather_description)
     print("Temp: " + str(celsius_temp))
     print("Feels like temp: " + str(feels_like_celsius_temp))
     print("Humidity: " + str(humidity))
-    print("Wind strength: " + str(wind_strength) +", " + "Wind direction: " + str(wind_direction))
+    print("Wind strength: " + str(wind_strength) + ", " + "Wind direction: " + str(wind_direction))
     print("Sunrise: " + str(json_dict['sys']['sunrise']))
     print("Sunset: " + str(json_dict['sys']['sunset']))
-
 
 def get_weather(location, api_key):
     import urllib.request
     weburl = urllib.request.urlopen(f"http://api.openweathermap.org/data/2.5/weather?q={location}&APPID={api_key}")
-    #print("result code: " + str(weburl.getcode()))
+    # print("result code: " + str(weburl.getcode()))
     data = weburl.read()
     return data
-
